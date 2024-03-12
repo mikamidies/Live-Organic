87cc5052cb66c5ec6ee3a56251663ff519902055
mikamidies <mikamidies04@gmail.com>
2024-03-13 00:01:26 +0500

    Update

---
+++ components/HomePage/HomeProducts.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="wrap">
+        <div class="container">
+            <h4 class="title section__title">
+                Products
+            </h4>
+            <div class="items">
+                <div class="item">
+                    <div class="badge">
+                        Save 11%
+                    </div>
+                    <div class="img">
+                        <img src="@/assets/img/product-1.png" alt="">
+                    </div>
+                    <div class="content">
+                        <p class="name">
+                            Apple
+                        </p>
+                        <p class="price">
+                            11$ for one KG
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="badge">
+                        Save 11%
+                    </div>
+                    <div class="img">
+                        <img src="@/assets/img/product-2.png" alt="">
+                    </div>
+                    <div class="content">
+                        <p class="name">
+                            Apple
+                        </p>
+                        <p class="price">
+                            11$ for one KG
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="badge">
+                        Save 11%
+                    </div>
+                    <div class="img">
+                        <img src="@/assets/img/product-3.png" alt="">
+                    </div>
+                    <div class="content">
+                        <p class="name">
+                            Apple
+                        </p>
+                        <p class="price">
+                            11$ for one KG
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="badge">
+                        Save 11%
+                    </div>
+                    <div class="img">
+                        <img src="@/assets/img/product-4.png" alt="">
+                    </div>
+                    <div class="content">
+                        <p class="name">
+                            Apple
+                        </p>
+                        <p class="price">
+                            11$ for one KG
+                        </p>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="badge">
+                        Save 11%
+                    </div>
+                    <div class="img">
+                        <img src="@/assets/img/product-5.png" alt="">
+                    </div>
+                    <div class="content">
+                        <p class="name">
+                            Apple
+                        </p>
+                        <p class="price">
+                            11$ for one KG
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.wrap {
+    padding: 80px 0;
+}
+
+.items {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 16px;
+}
+
+.item {
+    border: 1px solid #ebebeb;
+    position: relative;
+}
+
+.badge {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: red;
+    color: white;
+    padding: 4px 8px;
+}
+
+.img img {
+    width: 100%;
+    height: 280px;
+    object-fit: contain;
+    display: flex;
+}
+
+.content {
+    padding: 24px;
+}
+
+.name {
+    font-size: 24px;
+    color: var(--dark);
+    font-family: var(--decor-sm);
+    margin-bottom: 12px;
+}
+
+.price {
+    font-size: 16px;
+    line-height: 150%;
+    color: var(--green);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,10 @@
       <HomeHero />
       <HomeAbout />
       <HomeServices />
+      <HomeProducts />
+      <HomeForm />
     </main>
+    <DesktopFooter />
   </div>
 </template>
 
@@ -14,9 +17,12 @@ import DesktopNavbar from "@/components/DesktopNavbar.vue";
 import HomeHero from "@/components/HomePage/HomeHero.vue";
 import HomeAbout from "@/components/HomePage/HomeAbout.vue";
 import HomeServices from "@/components/HomePage/HomeServices.vue";
+import HomeProducts from "@/components/HomePage/HomeProducts.vue"
+import HomeForm from "@/components/HomePage/HomeForm.vue"
+import DesktopFooter from "@/components/DesktopFooter.vue"
 
 export default {
-  components: { DesktopNavbar, HomeHero, HomeAbout, HomeServices },
+  components: { DesktopNavbar, HomeHero, HomeAbout, HomeServices, HomeProducts, HomeForm, DesktopFooter },
 };
 </script>
 
